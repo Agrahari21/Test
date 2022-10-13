b5957fa6ad65cdaeaab226d8827d4328e4527b80
Rajesh <rajesh_agrahari@rediff.com>
2022-10-13 18:11:19 +0530

    First Version

---
+++ client/build/precache-manifest.d977aa228121064ddaa93b368eacbd8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff16e64fcc44459dbc07f2360733d1c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9fa2df4035a7a91ff40",
+    "url": "/static/js/2.a5dec339.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5dec339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5f998700dab7b706ec",
+    "url": "/static/js/main.f457d210.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
